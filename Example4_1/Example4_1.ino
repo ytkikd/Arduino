2269e897c16e947128850c86cb6d19481132a22b
Yutaka IKEDA <y_ikeda@mub.biglobe.ne.jp>
2017-07-16 11:52:10 +0900

    LED出力を記述

--- Example4_1/Example4_1.ino
+++ Example4_1/Example4_1.ino
@@ -1,11 +1,19 @@
+// Blinking LED
 
 
+const int LED = 13; // LEDはデジタルピン 13に接続
+
+// 初期化
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED, OUTUT); // 出力設定
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  digitalwrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000;
 }
