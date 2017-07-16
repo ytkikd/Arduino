45964e8f01314acd80df81f4c1076273651e869d
Yutaka IKEDA <y_ikeda@mub.biglobe.ne.jp>
2017-07-16 12:05:40 +0900

    LED点滅のスケッチ記述

--- Example4_1/Example4_1.ino
+++ Example4_1/Example4_1.ino
@@ -1,19 +1,17 @@
 // Blinking LED
 
-
 const int LED = 13; // LEDはデジタルピン 13に接続
 
-// 初期化
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED, OUTUT); // 出力設定
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalwrite(LED, HIGH);
+  digitalWrite(LED, HIGH);
   delay(1000);
   digitalWrite(LED, LOW);
-  delay(1000;
+  delay(1000);
 }
