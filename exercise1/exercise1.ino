f4db905b8c892f53bc0be85fcede0c8d1376f9c6
Yutaka IKEDA <y_ikeda@mub.biglobe.ne.jp>
2017-07-18 14:09:35 +0900

    LEDの青、黄、赤をリズムをつけて点滅

---
+++ exercise1/exercise1.ino
@@ -0,0 +1,40 @@
+// Blinking LED
+// LEDのフェードインとフェードアウト
+
+const int LED9 =  9; // 青色LED接続
+const int LED10 = 10; // 黄色LED接続
+const int LED11 = 11; // 赤色LED接続
+
+int i = 0;         // カウントアップ用の変数
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED9, OUTPUT);
+  pinMode(LED10, OUTPUT);
+  pinMode(LED11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(LED9, HIGH);
+  delay(500);
+
+  digitalWrite(LED10, HIGH);
+  delay(500);
+
+  digitalWrite(LED9, LOW);
+  delay(500);
+
+  digitalWrite(LED11, HIGH);
+  delay(500);
+
+  digitalWrite(LED10, LOW);
+  delay(500); 
+
+  digitalWrite(LED9, HIGH);
+  delay(500);
+
+  digitalWrite(LED11, LOW);
+  delay(500);     
+}
