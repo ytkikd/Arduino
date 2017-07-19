3be3bbf6d5cc7766ab65c5e6407e31d1caad9cd4
Yutaka IKEDA <y_ikeda@mub.biglobe.ne.jp>
2017-07-19 10:07:38 +0900

    2017/07/19 LED青のみ点滅、1秒点灯、3秒消灯の繰り返し

---
+++ exercise1_2_LED_blue/exercise1_2_LED_blue.ino
@@ -0,0 +1,26 @@
+// Blinking LED
+// LEDのフェードインとフェードアウト
+
+const int LED9 =  9; // 青色LED接続
+const int LED10 = 10; // 黄色LED接続
+const int LED11 = 11; // 赤色LED接続
+
+int i = 0;         // カウントアップ用の変数
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED9, OUTPUT);
+  pinMode(LED10, OUTPUT);
+  pinMode(LED11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(LED9, HIGH);
+  delay(1000);
+
+  digitalWrite(LED9, LOW);
+  delay(3000);
+
+}
